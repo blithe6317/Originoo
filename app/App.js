1b3afae3ea8e241c67a7f2f67650e52eb1a0bfdc
blithe6317 <1160457037@qq.com>
2018-11-08 16:27:45 +0800

    添加 目录及eslint

--- app/App.js
+++ app/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
+
     render() {
         return (
             <h3>Originoo</h3>
