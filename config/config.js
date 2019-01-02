f9071a2e81eefc379841acbea86a7f5145e4a9b2
blithe6317 <1160457037@qq.com>
2019-01-02 16:23:16 +0800

    配置文件更新

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ export default {
     plugins: [
         ['umi-plugin-react', {
             antd: true,
-            dva: false
+            dva: false,
+            dynamicImport: true,
         }],
     ],
     cssLoaderOptions: {
@@ -16,8 +17,56 @@ export default {
         component: './index',
         routes: [
             {
-                path: '/',
+                path: '/index',
                 component: 'Home/index',
+            }, {
+                path: '/user-center',
+                component: 'Center/layout',
+                routes: [{
+                    path: '/user-center/center',
+                    component: 'Center/index'
+                }, {
+                    path: '/user-center/downloadlog',
+                    component: 'Center/DownLoadLog'
+                }, {
+                    path: '/user-center/collection',
+                    component: 'Center/Collection'
+                }, {
+                    path: '/user-center/follow',
+                    component: 'Center/Follow'
+                }, {
+                    path: '/user-center/browse',
+                    component: 'Center/Browse'
+                }, {
+                    path: '/user-center/combo',
+                    component: 'Center/Combo'
+                }, {
+                    path: '/user-center/order',
+                    component: 'Center/Order'
+                }, {
+                    path: '/user-center/invoice',
+                    component: 'Center/Invoice'
+                }, {
+                    path: '/user-center/recharge',
+                    component: 'Center/Recharge'
+                }, {
+                    path: '/user-center/coupon',
+                    component: 'Center/Coupon'
+                }, {
+                    path: '/user-center/author',
+                    component: 'Center/Author'
+                }, {
+                    path: '/user-center/setting',
+                    component: 'Center/Setting'
+                }, {
+                    path: '/user-center/msg',
+                    component: 'Center/Msg'
+                }]
+            }, {
+                path: '/piclist',
+                component: 'PicList/index',
+            }, {
+                component: './404',
             }
         ]
     }],
